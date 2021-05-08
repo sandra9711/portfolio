4e6121fa70dda82a90019418dd15a9b45168467d
SANDRA <sandrarehan97@gmail.com>
2021-05-08 13:04:35 +0530

    Clean Repo

---
+++ src/mock/data.js
@@ -0,0 +1,78 @@
+import { nanoid } from 'nanoid';
+
+// HEAD DATA
+export const headData = {
+  title: '', // e.g: 'Name | Developer'
+  lang: '', // e.g: en, es, fr, jp
+  description: 'Welcome to my website', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: '',
+  name: 'Sandra',
+  subtitle: 'CS Undergrad',
+  cta: '',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'linpr.jpg',
+  paragraphOne: 'Hello, I am 2nd year student (Sophomore), pursuing B.tech in Computer Science Engineering from Mar Athanasius College of Engineering.',
+  paragraphTwo: ' I describe myself as a quick learner and passionate in problem solving by using simple and scalable solutions.',
+  paragraphThree: 'Interested in web development and machine learning.',
+  resume: '', // if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'lp.png',
+    title: 'Librosopedia',
+    info: 'An online used book store',
+    info2: 'A platform to promote the resuse of books',
+    url: 'https://librosopedia.web.app/',
+    repo: 'https://github.com/sandra9711/.hack-Librosopedia', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'jokegen.jpg',
+    title: 'Random joke generator',
+    info: 'Simple app to display random Chuck Norris facts',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/sandra9711/Random-Joke-generator', // if no repo, the button will not show up
+  },
+  
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: '',
+  btn: '',
+  email: 'sandrarehan97@gmail.com',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: nanoid(),
+      name: 'twitter',
+      url: 'https://twitter.com/sandra_mariyam',
+    },
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/sandra-mariyam-jacob-2689641b2/',
+    },
+    {
+      id: nanoid(),
+      name: 'github',
+      url: 'https://github.com/sandra9711',
+    },
+  ],
+};
+
+
